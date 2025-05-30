6e26eac9a83b30d76babfa24c1ce2c87763adb62
Aakanksha Ghodki <aakankshaghodki@gmail.com>
2025-05-30 23:32:55 +0530

    commit4

--- server/config/db.js
+++ server/config/db.js
@@ -1,41 +1,14 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+const connectDB = async()=>{
+    try{
+        mongoose.connection.on('connected',()=>console.log("Database Connected"));
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-        
-        mongoose.connection.on('error', (err) => {
-            console.error('MongoDB connection error:', err);
-        });
-
-        mongoose.connection.on('disconnected', () => {
-            console.log('MongoDB disconnected');
-        });
-
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-        process.exit(1);
+        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
     }
-};
+    catch(error){
+        console.log(error.message);
+    }
+}
 
-export default connectDB;
-
-// import mongoose from "mongoose";
-
-// const connectDB = async()=>{
-//     try{
-//         mongoose.connection.on('connected',()=>console.log("Database Connected"));
-
-//         await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
-//     }
-//     catch(error){
-//         console.log(error.message);
-//     }
-// }
-
-// // export 
+export default connectDB
