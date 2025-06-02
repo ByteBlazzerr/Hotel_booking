6a3d2dce2ec5b72badb92fda7436e6e8a1c148d2
Aakanksha Ghodki <aakankshaghodki@gmail.com>
2025-06-02 11:28:19 +0530

    in stripeWebhooks make header to headers

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -7,7 +7,7 @@ const stripeInstance =new stripe(process.env.STRIPE_SECRET_KEY);
 export const stripeWebhooks=async(request,response)=>{
     // Stripe Gateway Initialize
 
-    const sigd=request.header['stripe-signature'];
+    const sigd=request.headers['stripe-signature'];
 
     let event;
     try{
